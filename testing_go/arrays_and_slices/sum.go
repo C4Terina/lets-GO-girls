08579eac612a1070fa165c6a11d88061e031d0d6
C4Terina <kapa97531@gmail.com>
2023-10-19 13:45:19 +0300

    end chapter

--- testing_go/arrays_and_slices/sum.go
+++ testing_go/arrays_and_slices/sum.go
@@ -1,3 +1,25 @@
-package arraysandslices
+package main
 
-func Sum()
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAllTails(numbers2Sum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbers2Sum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
--- testing_go/arrays_and_slices/sum_test.go
+++ testing_go/arrays_and_slices/sum_test.go
@@ -1,12 +1,54 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestSum(t *testing.T){
-	numbers := [5]int{1,2,3,4,5}
-	got := Sum(numbers)
-	want := 15 
+func TestSum(t *testing.T) {
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
 
-	if got != want 
-		t.Errorf("got %d want %d given %v",got, want, numbers)
+		if got != want {
+			t.Errorf("got %d want %d given %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of any size array", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given %v", got, want, numbers)
+		}
+
+	})
+
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	t.Run("collection of tails for not empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("collection of tails for empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+	})
 }
